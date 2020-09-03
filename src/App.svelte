a20621e19897ca6bacb2c9e9fbce3aaa356ed9ed
msakuta <masahiro.sakuta@gmail.com>
2020-09-04 01:23:01 +0900

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	let cells = [[]]
+	for(let i = 0; i < 10; i++){
+		let row = []
+		for(let j = 0; j < 10; j++){
+			row.push(Math.random() < 0.02 ? "1" : "0")
+		}
+		cells.push(row)
+	}
+</script>
+
+<h1>EDT sweeper in Svelte</h1>
+
+<div style="position: relative">
+	{#each cells as row, i}
+		{#each row as cell, j}
+			<div class="cell" style="left: {j * 30}px; top: {i * 30}px; width: 30px; height: 30px; ">
+				{cell}
+			</div>
+		{/each}
+	{/each}
+</div>
+
+<style>
+	.cell{
+		text-align: center;
+		position: absolute;
+		border: solid 1px black;
+	}
+</style>
